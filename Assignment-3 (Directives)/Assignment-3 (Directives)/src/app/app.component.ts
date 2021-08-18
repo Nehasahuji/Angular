a60a9e8e46deb272e692b99c5ee766e88a2de88c
Neha Sahu <nehasahu9424@gmail.com>
2021-08-18 12:50:48 +0530

    Added Directive assignment

---
+++ Assignment-3 (Directives)/Assignment-3 (Directives)/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  secretPwd: boolean = false;
+  log=[];
+
+
+  onToggleDetails(){
+    this.secretPwd = !this.secretPwd;
+    this.log.push(this.log.length + 1);
+    // this.log.push(new Date);
+  
+  }
+
+
+
+}
